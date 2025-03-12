c1b6a232eb4b7ada41e49c92d5f0a94aa928de90
Aneesh Singh Rajoriya <aneeshsinghrajoriya@gmail.com>
2025-03-12 18:21:07 +0530

    Added Skeleton Routes to Backend

---
+++ backend/routes/course.js
@@ -0,0 +1,13 @@
+const {Router}=require('express');
+
+const courseRouter= Router();
+
+courseRouter.get('/preview',function(req,res){
+
+})
+courseRouter.get('/purchase',function(req,res){
+    
+})
+module.exports={
+    courseRouter: courseRouter
+}
---
+++ backend/routes/user.js
@@ -0,0 +1,16 @@
+const {Router}=require('express');
+
+const userRouter= Router();
+
+userRouter.post('/signup',async function(req,res){
+
+})
+userRouter.post('/signin',async function(req,res){
+    
+})
+userRouter.get('/purchases',async function(req,res){
+    
+})
+module.exports={
+    userRouter: userRouter
+}
