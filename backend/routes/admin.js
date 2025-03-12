2af4a08a4cbcf566ad0e10cd248505a2ef7007c7
Aneesh Singh Rajoriya <aneeshsinghrajoriya@gmail.com>
2025-03-12 18:43:00 +0530

    adding admin routes

---
+++ backend/routes/admin.js
@@ -0,0 +1,18 @@
+const {Router}=require('express');
+const adminRouter=Router();
+
+adminRouter.post('/signup',async function(req,res){
+
+})
+adminRouter.post('/signin',async function(req,res){
+    
+})
+adminRouter.post('/course',async function(req,res){
+
+})
+adminRouter.put('/course',async function(req,res){
+
+})
+module.exports={
+    adminRouter: adminRouter
+}
