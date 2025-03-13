9f03474a4374d8f71708ac285bb954cb043c620b
Aneesh Singh Rajoriya <aneeshsinghrajoriya@gmail.com>
2025-03-13 11:11:20 +0530

    Adding Routes and env files

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,20 @@
+require('dotenv').config()
 const express=require('express');
 const {userRouter}=require('./routes/user');
 const {courseRouter}=require('./routes/course')
 const {adminRouter}=require('./routes/admin');
+const mongoose = require('mongoose');
 const app=express();
-
+app.use(express.json());
 app.use('/user',userRouter);
 app.use('/admin',adminRouter);
 app.use('/course',courseRouter);
 
-app.listen(3000,()=>{
-    console.log("Running on port 3000");
-})
+async function main(){
+    await mongoose.connect(process.env.MONGOOSE_URL);
+    console.log("Connected To Database");
+    app.listen(3000,()=>{
+        console.log("Running on port 3000");
+    })
+}
+main();
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,11 +1,72 @@
 const {Router}=require('express');
 const adminRouter=Router();
+const {adminModel}=require('../db');
+const {z}=require('zod');
+const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
 
 adminRouter.post('/signup',async function(req,res){
-
+    const {email,password,firstName,lastName}=req.body;
+    const bodySchema=z.object({
+        email : z.string().email(),
+        password: z.string().min(8).max(20).regex(/[A-Z]/).regex(/[a-z]/),
+        firstName: z.string().min(4).max(20),
+        lastName: z.string().min(4).max(20)
+    });
+    const verify=bodySchema.safeParse(req.body);
+    if(verify.success){
+        const hashPassword=await bcrypt.hash(password,5);
+       try{
+          await adminModel.create({
+                email : email,
+                password: hashPassword,
+                firstName: firstName,
+                lastName: lastName
+            })
+            res.json({
+                msg: "You have Signuped Successfully"
+            })
+        }
+        catch(err){
+            res.json({
+                msg : "There is Some error"
+            })
+        }
+    }
+    else{
+        res.json(verify.error);
+    }
 })
 adminRouter.post('/signin',async function(req,res){
-    
+    const {email,password}=req.body;
+    const bodySchema=z.object({
+        email : z.string().email(),
+        password: z.string().min(8).max(20).regex(/[A-Z]/).regex(/[a-z]/)
+    });
+    const verify=bodySchema.safeParse(req.body);
+    if(verify.success){
+        const user= await adminModel.findOne({
+            email: email
+        })
+        const compare=await bcrypt.compare(password,user.password);
+        if(compare){
+            const token=jwt.sign({
+                token : user._id.toString()
+            },"COURSE");
+            res.headers.token=token;
+            res.json({
+                token : token
+            });
+        }
+        else{
+            res.json({
+                msg : "Wrong Crendentials"
+            })
+        }
+    }
+    else{
+        res.json(verify.error);
+    }
 })
 adminRouter.post('/course',async function(req,res){
 
@@ -13,6 +74,10 @@ adminRouter.post('/course',async function(req,res){
 adminRouter.put('/course',async function(req,res){
 
 })
+adminRouter.get('/course',async function(req,res){
+
+})
+
 module.exports={
     adminRouter: adminRouter
 }
